8ccda246f30607f762781ff93031e4aea79cad4c
Shigeru251 <tormspp@icloud.com>
2020-01-18 18:43:01 +0900

    Initial Commit

---
+++ heloword/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  heloword
+//
+//  Created by 荒木茂 on 2020/01/18.
+//  Copyright © 2020 shigeru.araki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
